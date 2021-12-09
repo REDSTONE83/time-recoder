449bbc2db59ea327c80e66be742c500c550154e6
REDSTONE83 <redstone8311@gmail.com>
2021-12-10 00:28:16 +0900

    Update date summary <pre>

--- src/views/Event.vue
+++ src/views/Event.vue
@@ -9,7 +9,7 @@
             {{ date.dateString }}
           </v-card-title>
           <v-card-subtitle v-if="date.summary">
-            {{ date.summary }}
+            <pre>{{ date.summary }}</pre>
           </v-card-subtitle>
           <v-card-actions>
             <v-btn icon @click="showDetailDate = showDetailDate === date.date ? '' : date.date">
